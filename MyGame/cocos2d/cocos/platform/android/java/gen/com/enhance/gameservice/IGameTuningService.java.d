61da83e50b76eea77aab3725aaca8047ce959171
lity_lee <tonglei.lee@gmail.com>
2016-12-30 13:39:23 +0800

    删除.idea配置文件

--- MyGame/cocos2d/cocos/platform/android/java/gen/com/enhance/gameservice/IGameTuningService.java.d
+++
@@ -1,3 +0,0 @@
-/media/d/projects/CocosGame/MyGame/cocos2d/cocos/platform/android/java/gen/com/enhance/gameservice/IGameTuningService.java: \
-  /media/d/projects/CocosGame/MyGame/cocos2d/cocos/platform/android/java/src/com/enhance/gameservice/IGameTuningService.aidl 
-
